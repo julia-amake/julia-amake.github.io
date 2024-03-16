967f80c7fded12f85610a4250c9a37a0cd0d982c
Yulia Starchenko <imay@amake.ru>
2024-03-16 15:53:58 +0300

    feat: updated global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'normalize.css';
 import './index.scss';
 import App from './App';
 import 'src/shared/config/i18n/i18n';
