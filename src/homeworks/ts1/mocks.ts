5400f37a4d51f150c10cd9a44835233c2210a55b
Yulia Starchenko <imay@amake.ru>
2024-04-07 18:13:57 +0300

    feat: changed logic for random product

---
+++ src/homeworks/ts1/mocks.ts
@@ -0,0 +1,52 @@
+export const categories = {
+  Лицо: 'https://www.azyaamode.com/files/stories/group0/original/shutterstock_318602609.jpg',
+  Волосы: 'https://brando-s.ru/wp-content/uploads/maska_uhod.jpg',
+  Тело: 'https://avatars.dzeninfra.ru/get-zen_doc/9348320/pub_641221a5aad9141a24c137d7_641221c6aad9141a24c13cf0/scale_1200',
+  Макияж:
+    'https://mykaleidoscope.ru/uploads/posts/2023-12/thumbs/1702576373_mykaleidoscope-ru-p-makiyazh-s-sinimi-strelkami-zhenskie-pinte-49.jpg',
+  Парфюмерия: 'https://i.pinimg.com/originals/dd/da/67/ddda678d0385a6f5a398fdeaf59201be.png',
+};
+
+export const products = {
+  Лицо: [
+    'https://a.lmcdn.ru/product/M/P/MP002XW0OSUO_22397951_1_v3.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XW015OP_19346672_1_v2.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XU04J8X_15194473_1_v3.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLACM248901_19411538_1_v1.jpg',
+    'https://a.lmcdn.ru/product/V/I/VI055LWAWTO1_6311654_1_v6.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADG778701_22406684_1_v1.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XW0KN6N_17928069_1_v2.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XU0D02C_21458687_1_v2.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XW0CFGG_16832490_1_v3.jpg',
+  ],
+  Волосы: [
+    'https://a.lmcdn.ru/product/M/P/MP002XW0O8B3_18948723_1_v2.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLACY323201_20979339_1_v1.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLACV381701_20595075_1_v1.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLABM261401_17259413_1_v1.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADG947601_22561495_1_v1.jpg',
+  ],
+  Тело: [
+    'https://a.lmcdn.ru/product/R/T/RTLADI371401_22757047_1_v1.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLACL039801_19275525_1_v1.jpg',
+    'https://a.lmcdn.ru/product/V/I/VI055LWISAE9_10606926_1_v2.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADG605901_22458285_1_v1.jpg',
+    'https://a.lmcdn.ru/product/V/I/VI055LWJARF0_10820103_1_v2.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XW0MBUU_18683676_1_v2.jpg',
+  ],
+  Макияж: [
+    'https://a.lmcdn.ru/product/R/T/RTLABC980601_16333806_1_v1.jpg',
+    'https://a.lmcdn.ru/product/C/L/CL035LWKOCU0_11926429_1_v2.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XW0OSUC_22387271_1_v2.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADJ613801_22964810_1_v1.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADK873201_22979788_1_v1.jpg',
+  ],
+  Парфюмерия: [
+    'https://a.lmcdn.ru/product/B/Y/BY007LUUQU29_4983199_1_v2.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLACS116901_20213518_1_v1.jpg',
+    'https://a.lmcdn.ru/product/D/G/DG577LWBDK59_1445867_1_v3.jpg',
+    'https://a.lmcdn.ru/product/R/T/RTLADJ655901_22830067_1_v1.jpg',
+    'https://a.lmcdn.ru/product/M/P/MP002XU05HAM_21699658_1_v1.jpg',
+    'https://a.lmcdn.ru/product/C/A/CA251LWGZN03_2917980_1_v3.jpg',
+  ],
+};
