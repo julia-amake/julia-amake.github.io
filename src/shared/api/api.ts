3420463a618c2157ed68312eba9c1b1a0b76fd5a
Yulia Starchenko <imay@amake.ru>
2024-08-04 14:28:27 +0300

    fix: replaced local getAuthorizationToken

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { API_URL } from 'src/shared/consts/api';
-import { LOCAL_STORAGE_TOKEN_KEY } from 'src/shared/consts/localStorage';
-
-const getAuthorizationToken = () => `Bearer ${localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY) || ''}`;
+import { getAuthorizationToken } from 'src/shared/lib/utils';
 
 export const $api = axios.create({
   baseURL: API_URL,
